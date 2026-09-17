46397615ffbe1476a53d5ffc7b51524d4d8a8994
Teddybar5 <123429017+Teddybar5@users.noreply.github.com>
2026-09-16 18:10:02 -0700

    Simple Game loop
    Added a simple game loop where you talk to the npc, go up and pick flowers, and then return to the npc.

--- froggys-big-day/scripts/npc.gd
+++ froggys-big-day/scripts/npc.gd
@@ -1,12 +1,17 @@
 class_name NPC extends CharacterBody2D
 
 var player_in_range = false
+var has_talked = false
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var dialogue_box: RichTextLabel = $"../DialogueBox"
+@onready var player_character: Player = $"../PlayerCharacter"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animation_player.play("idle")
+	dialogue_box.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,8 +25,17 @@ func _on_interaction_area_body_exited(body: Node2D) -> void:
 	player_in_range = false
 	
 func _input(event: InputEvent) -> void:
-	if player_in_range and event.is_action_pressed("interact"):
+	if player_in_range and event.is_action_pressed("interact") and !has_talked:
 		run_dialogue()
+	elif  player_in_range and event.is_action_pressed("interact") and has_talked:
+		check_flowers()
 	
 func run_dialogue() -> void:
-	print("Hello, traveler!")
+	dialogue_box.show()
+	has_talked = true
+	
+func check_flowers() -> void:
+	if player_character.flowers >= 5:
+		dialogue_box.text = "Yay, Thank you! You win!"
+	else:
+		dialogue_box.text = "Not enough!"
