758d3ca06b29f5a76a330fc09087f8072b5aafba
Teddybar5 <123429017+Teddybar5@users.noreply.github.com>
2026-09-24 19:14:25 -0700

    Changed viewport and edited main level

--- froggys-big-day/scripts/state_walk.gd
+++ froggys-big-day/scripts/state_walk.gd
@@ -1,6 +1,6 @@
 class_name Walk extends State
 
-@export var move_speed : float = 100.0
+@export var move_speed : float = 150.0
 @onready var idle: Idle = $"../Idle"
 
 ## what happens when the player enters this state
